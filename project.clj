aa1479abea32b16c179d6a35e0727ddd9d9ff1c5
Brian Guthrie <brian.guthrie@soundcloud.com>
2015-04-27 09:08:58 +0200

    Small tweak to the project in preparation for a release.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject shrubbery "0.1.0-SNAPSHOT"
-  :description "Stub and spy on protocols."
+(defproject com.gearswithingears/shrubbery "0.1.0"
+  :description "A stubbing, spying, and mocking library for Clojure protocols."
   :url "http://github.com/bguthrie/shrubbery"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
