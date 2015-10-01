97d980689c217550933002cb1832330636dd9d5e
Brian Guthrie <brian.guthrie@soundcloud.com>
2015-09-30 23:25:34 -0400

    We apparently don't support 1.4.

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev [:test {:dependencies [[org.clojure/clojure "1.7.0"]]}]
              :1.6 [:test {:dependencies [[org.clojure/clojure "1.6.0"]]}]
-             :1.5 [:test {:dependencies [[org.clojure/clojure "1.5.1"]]}]
-             :1.4 [:test {:dependencies [[org.clojure/clojure "1.4.0"]]}]})
+             :1.5 [:test {:dependencies [[org.clojure/clojure "1.5.1"]]}]})
